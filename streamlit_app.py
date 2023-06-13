3820e06287e9cc5c38eee8d1733bbf15fcc9df26
Anders <78318530+anderseline@users.noreply.github.com>
2023-06-13 12:57:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas as pd
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('Menu')
 streamlit.header('Breakfast Menu')
